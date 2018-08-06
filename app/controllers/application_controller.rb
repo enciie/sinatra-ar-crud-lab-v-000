86fe7a3f0bf8941d4722d3cd64b8064f12da3143
Nancy Jo <enciie@gmail.com>
2018-08-06 01:45:42 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,5 +11,9 @@ class ApplicationController < Sinatra::Base
   get '/posts/new' do
     erb :new
   end
-  
+
+  post '/posts' do
+    @post = Post.create(params)
+    redirect to '/posts'
+  end
 end
